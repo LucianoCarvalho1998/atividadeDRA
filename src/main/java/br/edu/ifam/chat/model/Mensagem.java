72c549465ace271f39c34e8dc974ef653ab7c9d1
Luciano Alves Carvalho <89809732+LucianoCarvalho1998@users.noreply.github.com>
2023-09-20 20:49:08 -0400

    Atualizar o Mensagem.java

--- src/main/java/br/edu/ifam/chat/model/Mensagem.java
+++ src/main/java/br/edu/ifam/chat/model/Mensagem.java
@@ -1,5 +1,56 @@
 package br.edu.ifam.chat.model;
 
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Mensagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Calendar dataHora;
+	private String conteudo;
+	private Contato emissor;
+	private Contato receptor;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Calendar getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(Calendar dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	public Contato getEmissor() {
+		return emissor;
+	}
+	public void setEmissor(Contato emissor) {
+		this.emissor = emissor;
+	}
+	public Contato getReceptor() {
+		return receptor;
+	}
+	
+	public void setReceptor(Contato receptor) {
+		this.receptor = receptor;
+	}
+	
+	
 
 }
